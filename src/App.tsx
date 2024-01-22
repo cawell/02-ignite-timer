b75fbceadf9dc0ee9dcc9e776370117435c4be38
Wellington Caetano <wl.a.caetano@gmail.com>
2024-01-22 01:35:30 -0300

    feat: create default conf styles and router conf

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyles } from './styles/global'
+import { Router } from './Router.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
+export function App() {
   return (
-    <>
-      Hello Word
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
