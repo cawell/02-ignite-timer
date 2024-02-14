f35a7f7e59fca802bee9b59bf3e50168deb403c0
Wellington Caetano <wl.a.caetano@gmail.com>
2024-02-14 11:26:10 -0300

    revert: add default route navigation in Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
@@ -7,7 +7,6 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route index element={<Navigate replace to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
