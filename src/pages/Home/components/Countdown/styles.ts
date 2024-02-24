73f78872ae445e9bc3d336666b74af92ec557b57
Wellington Caetano <wl.a.caetano@gmail.com>
2024-02-24 00:08:58 -0300

    feat: update Countdown component styles
    The font size in the Countdown component has been adjusted from 10rem to 1rem. The styles for responsive design have also been updated to ensure better readability and placement of elements on screens at max-width of 720px. This update includes modifications like changing font size, padding, color, and gap between elements.

--- src/pages/Home/components/Countdown/styles.ts
+++ src/pages/Home/components/Countdown/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const CountdownContainer = styled.div`
   font-family: 'roboto Mono', monospace;
-  font-size: 10rem;
+  font-size: 1rem;
   color: ${(props) => props.theme['gray-100']};
 
   display: flex;
@@ -12,6 +12,11 @@ export const CountdownContainer = styled.div`
     background: ${(props) => props.theme['gray-700']};
     padding: 2rem 1rem;
     border-radius: 8px;
+    font-size: 15vw;
+  }
+
+  @media (max-width: 720px) {
+    gap: 0.5rem;
   }
 `
 
@@ -23,4 +28,10 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+
+  @media (max-width: 720px) {
+    width: 1rem;
+    padding: 15vw 0.5rem;
+    color: ${(props) => props.theme['gray-800']};
+  }
 `
